6633f493947751369a11ef286c9a6bb912ae7bbb
Barteł <bartekbrozek@gmail.com>
2017-10-02 17:22:49 +0200

    Login and register

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Facebook\Facebook;
+use Facebook\Exceptions;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class UserController extends Controller
+{
+
+    /**
+     * @Route("/login", name="login")
+     */
+//    public function indexAction(Request $request)
+//    {
+//        return $this->render('FOSUserBundle/views/layout.html.twig', [
+//
+//        ]);
+//    }
+
+}

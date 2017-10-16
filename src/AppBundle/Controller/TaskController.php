b94c1f8512aa72406219c35bb72996981cf82e02
Barteł <bartekbrozek@gmail.com>
2017-10-16 20:54:21 +0200

    Edit/Delete task, dismisseable msgs

--- src/AppBundle/Controller/TaskController.php
+++ src/AppBundle/Controller/TaskController.php
@@ -4,10 +4,10 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Task;
 use AppBundle\Form\TaskType;
-use Doctrine\Common\Collections\ArrayCollection;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class  TaskController extends Controller
@@ -16,7 +16,7 @@ class  TaskController extends Controller
     /**
      * @Route("/task/show/{id}", name="task")
      */
-    public function indexAction(Request $request, $id)
+    public function indexAction(Request $request, $id): Response
     {
         $task = $this->getDoctrine()->getRepository(Task::class)
             ->getSingleTaskById($id);
@@ -34,25 +34,66 @@ class  TaskController extends Controller
     /**
      * @Route("/task/create", name="taskCreate")
      */
-    public function createAction(Request $request)
+    public function createAction(Request $request): Response
     {
         $task = new Task();
         $form = $this->createForm(TaskType::class, $task);
-
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
 
             $em->persist($task);
             $em->flush();
 
-            $request->getSession()->getFlashBag()->add('success','Task added successfully');
+            $request->getSession()->getFlashBag()->add('success', 'Task added successfully');
             return $this->redirectToRoute('index');
         }
 
-        return $this->render('task/create.html.twig', [
+        return $this->render('task/createOrEdit.html.twig', [
+            'pageHeader' => 'Create Task',
             'form' => $form->createView()
         ]);
     }
 
+    /**
+     * @Route("/task/edit/{id}", name="taskUpdate")
+     */
+    public function editAction(Request $request, Task $task): Response
+    {
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            $request->getSession()->getFlashBag()->add('success', 'Task updated successfully');
+            return $this->redirectToRoute('taskUpdate', ['id' => $task->getId()]);
+        }
+
+        return $this->render('task/createOrEdit.html.twig', [
+            'pageHeader' => 'Edit Task',
+            'task' => $task,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/task/delete/{id}", name="taskDelete")
+     */
+    public function deleteAction(Request $request, Task $task): Response
+    {
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+            $em = $this->getDoctrine()->getManager();
+
+            $em->remove($task);
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add('success', 'Task removed successfully');
+            return $this->redirectToRoute('index');
+
+
+    }
 }
